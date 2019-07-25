8cf4e8c39490b57e1edfb6aae11b95ad17709c78
Daniel Ginn <daniel.ginn@live.com.au>
2019-07-25 16:45:49 +1000

    Load images and pose info function compiling

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import keras
+import scipy
 import matplotlib as plt
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.applications import ResNet50
@@ -14,32 +15,40 @@ from keras.optimizers import Adam
 import tensorflow as tf
 from tensorflow.python.client import device_lib
 from keras.preprocessing.image import load_img
+from scipy.spatial.transform import Rotation as R
 
 # print(device_lib.list_local_devices())
 def loadImages(numImages):
-    x = np.zeros((numImages,480,640,3))
-    y = np.zeros((numImages,4,4))
+    images = np.zeros((numImages,480,640,3))
+    xyz = np.zeros((numImages,3))
+    q = np.zeros((numImages,4))
     # load the image
     for i in range(numImages):
         # Load in image
         imageFileName = "./7scenes/chess/test/frame-{}.color.png".format(str(i).zfill(6))
         img = load_img(imageFileName)
-        x[i,:,:,:] = img_to_array(img)
+        images[i,:,:,:] = img_to_array(img)
 
         # Load in pose data
         poseFileName = "./7scenes/chess/test/frame-{}.pose.txt".format(str(i).zfill(6))
         file_handle = open(poseFileName, 'r')
         # Read in all the lines of your file into a list of lines
         lines_list = file_handle.readlines()
-        # Extract dimensions from first line. Cast values to integers from strings.
-        #cols, rows = (float(val) for val in lines_list[0].split())
-        # Do a double-nested list comprehension to get the rest of the data into your matrix
-        my_data = [[float(val) for val in line.split()] for line in lines_list[0:]]
+        # Do a double-nested list comprehension to store as a Homogeneous Transform matrix
+        homogeneousTransformList = [[float(val) for val in line.split()] for line in lines_list[0:]]
+        homogeneousTransform = np.zeros((4,4))
+
+        for j in range(4):
+            homogeneousTransform[j,:] = homogeneousTransformList[j]
+
+        # Extract rotation from homogeneous Transform
+        r = R.from_dcm(homogeneousTransform[0:3,0:3])
+        q = r.as_quat()
+        # Extract xyz from homogeneous Transform
+        xyz[i,:] = homogeneousTransform[0:3,3]
+
         file_handle.close()
-
-        print(my_data)
-
-    return x,y
+    return images,xyz,q
 
 base_model = ResNet50(weights='imagenet',include_top=False)
 
@@ -47,15 +56,15 @@ x = base_model.output
 x = GlobalAveragePooling2D()(x)  # **** Assuming 2D, with no arguments required
 x = Dense(1024, activation='relu', name='fc1')(x)  # **** Assuming relu
 xyz = Dense(3, activation='softmax', name='xyz')(x)  # **** Assuming softmax is the correct activation here
-rtp = Dense(4, activation='softmax', name='rtp')(x)  # **** Assuming softmax (rho/theta/phi) and quaternians
+q = Dense(4, activation='softmax', name='q')(x)  # **** Assuming softmax (rho/theta/phi) and quaternians
 
-global_pose_network = Model(inputs=base_model.input, outputs=[xyz, rtp])
+global_pose_network = Model(inputs=base_model.input, outputs=[xyz, q])
 
 global_pose_network.compile(optimizer='Adam',loss='mean_squared_error')
 #global_pose_network.summary()
 
 
-x_train, y_train = loadImages(2)
+x_train, y_xyz_train, y_q_train = loadImages(2)
 print("Finished Successfully")
 #train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input) #included in our dependencies
 
