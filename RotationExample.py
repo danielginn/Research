969f2bbe897549482805da77cc307140824defe9
Daniel Ginn <daniel.ginn@live.com.au>
2020-05-04 10:29:50 +1000

    Improving Rotation Example

--- RotationExample.py
+++ RotationExample.py
@@ -11,6 +11,7 @@ R3 = R.from_euler('zyx',[48, 0, 0], degrees=True)
 R4 = R.from_euler('zyx',[50, 0, 0], degrees=True)
 R5 = R.from_euler('zyx',[52, 0, 0], degrees=True)
 R6 = R.from_euler('zyx',[54, 0, 0], degrees=True)
+R7 = R.from_euler('zyx',[56, 0, 0], degrees=True)
 
 q1 = R1.as_quat()
 q2 = R2.as_quat()
@@ -18,29 +19,34 @@ q3 = R3.as_quat()
 q4 = R4.as_quat()
 q5 = R5.as_quat()
 q6 = R6.as_quat()
+q7 = R7.as_quat()
 
-q1array = np.zeros((5, 7))
-q2to6 = np.zeros((5, 7))
+q1array = np.zeros((6, 4))
+q2to7 = np.zeros((6, 4))
 
-q1array[0, 3:7] = q1
-q1array[1, 3:7] = q1
-q1array[2, 3:7] = q1
-q1array[3, 3:7] = q1
-q1array[4, 3:7] = q1
+q1array[0, :] = q1
+q1array[1, :] = q1
+q1array[2, :] = q1
+q1array[3, :] = q1
+q1array[4, :] = q1
+q1array[5, :] = q1
 
-q2to6[0, 3:7] = q2
-q2to6[1, 3:7] = q3
-q2to6[2, 3:7] = q4
-q2to6[3, 3:7] = q5
-q2to6[4, 3:7] = q6
+q2to7[0, :] = q2
+q2to7[1, :] = q3
+q2to7[2, :] = q4
+q2to7[3, :] = q5
+q2to7[4, :] = q6
+q2to7[5, :] = q7
 
-#print(q2to6)
+#print(q2to7)
 
-a = Input(shape=(7,))
-model = Model(inputs=a, outputs=a)
+a = Input(shape=(4,))
+b = Dense(4, name='myoutput')(a)
+model = Model(inputs=a, outputs=b)
 #for layer in model.layers:
 #    layer.trainable = False
 
-model.compile(optimizer=Adam(lr=1e-4, epsilon=1e-10), loss='mean_squared_error', metrics=[CustomImageGen.q_error])
-result = model.evaluate(x=q2to6, y=q1array)
-print(result)
+model.compile(optimizer=Adam(lr=1e-4, epsilon=1e-10), loss='mean_squared_error', metrics=['mae'])
+metrics = CustomImageGen.Metrics()
+model.fit(x=q2to7, y=q1array, batch_size=2, epochs=1, verbose=2, callbacks=[metrics], validation_data=(q2to7, q1array))
+print(metrics.losses)
